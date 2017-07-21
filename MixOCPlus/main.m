db58cd30a3d4eb4b78e06640a71c55ad54c237e1
Kong Lee <konglee873@gmail.com>
2017-07-21 09:50:20 +0800

    Initial Commit

---
+++ MixOCPlus/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MixOCPlus
+//
+//  Created by kong on 2017/7/21.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
